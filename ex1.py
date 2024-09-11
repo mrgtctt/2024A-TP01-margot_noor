4c13035e3557d6af77bfc5e49117526cda9c132b
margot cottreau <margot-loane.salmon--cottreau@polymtl.ca>
2024-09-11 17:25:58 -0400

    exercice 1 et exercice 3

--- ex1.py
+++ ex1.py
@@ -1,5 +1,13 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
-country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+country = input("De quelle nationalité est l'athlète ? \n")
+athlete = input("Quel est le nom de l'athlète ? \n")
+date = input("Quelle est la date du record ? \n")
+discipline = input("Quelle est la discipline du record ? \n")
+catégorie = input("Quelle est la catégorie du record ? \n")
+record = input("Quel est le record ? \n")
+
+if catégorie.isalpha(): 
+    print("Nouveau Record : \n-------------------- \n ",date, "\n",discipline,  "-",catégorie, "\n",athlete, country, "-",record)
+else : 
+    print("Nouveau Record : \n -------------------- \n ",date, "\n",discipline, "\n", athlete, country, "-",record) 
