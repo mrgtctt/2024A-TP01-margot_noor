46ae2620cacb65c812516cfded75f57306b5d487
margot cottreau <margot-loane.salmon--cottreau@polymtl.ca>
2024-09-11 22:13:42 -0400

    exercice 5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,14 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input("Quel est le pays concerné ? \n")
+code_medals = str(input("Veuillez entrer les résultats ? \n"))
+l = len(code_medals)
+nbr_g = code_medals.count("G")
+nbr_s = code_medals.count("S")
+nbr_b = code_medals.count("B")
+nbr_tot = nbr_g + nbr_s + nbr_b 
+if l == nbr_tot : 
+    print("Medailles : \n - Or : ", nbr_g, "\n - Argent : ", nbr_s, "\n - Bronze :", nbr_b)
+else : 
+    print("Veuillez saisir une chaîne valide.")
